779e84d76d36ca094a2283b5db9f2cde5693fb4a
Woefey <90526123+Woefey@users.noreply.github.com>
2023-01-24 19:44:56 +0100

    Add files via upload

---
+++ client/main.lua
@@ -0,0 +1,43 @@
+local function DrawText3Ds(x, y, z, text)
+	SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(true)
+    AddTextComponentString(text)
+    SetDrawOrigin(x,y,z, 0)
+    DrawText(0.0, 0.0)
+    local factor = (string.len(text)) / 370
+    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
+    ClearDrawOrigin()
+end
+
+local leeggoedlocatie = vector3(-1559.45, -432.24, 42.10)
+local sl = leeggoedlocatie
+
+CreateThread(function()
+    while true do
+        local InRange = false
+        local PlayerPed = PlayerPedId()
+        local PlayerPos = GetEntityCoords(PlayerPed)
+
+            local dist = #(PlayerPos - vector3(sl.x, sl.y, sl.z)) 
+            if dist < 10 then
+                InRange = true
+                DrawMarker(2,sl.x, sl.y, sl.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.2, 0.1, 255, 0, 0, 155, 0, 0, 0, 1, 0, 0, 0)
+                if dist < 1 then
+
+                    DrawText3Ds(sl.x, sl.y, sl.z, '~g~E~w~ - om je leeggoed te verkopen.') 
+                    if IsControlJustPressed(0, 38) then
+                        TriggerServerEvent('woefey-leeggoed:server:sellleeggoed')
+                    end
+                end
+            end
+
+        if not InRange then
+            Wait(5000)
+        end
+        Wait(5)
+    end
+end)
---
+++ server/main.lua
@@ -0,0 +1,38 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local Leeggoedlijst = {
+   
+    ["leeggoed_lg1"]  =  1,
+    ["leeggoed_lg2"]  =  1,
+    ["leeggoed_lg3"]  =  1,
+    ["leeggoed_lg4"]  =  1,
+    ["leeggoed_lg5"]  =  1,
+    ["leeggoed_lp1"]  =  1,
+    ["leeggoed_lp2"]  =  1,
+    ["leeggoed_lp3"]  =  1,
+    ["leeggoed_lp4"]  =  1,
+}
+
+RegisterNetEvent('woefey-leeggoed:server:sellleeggoed', function()
+    local src = source
+    local price = 0
+    local Player = QBCore.Functions.GetPlayer(src)
+    
+    local xItem = Player.Functions.GetItemsByName(Leeggoedlijst)
+    if xItem ~= nil then
+        for k, v in pairs(Player.PlayerData.items) do
+            if Player.PlayerData.items[k] ~= nil then
+                if Leeggoedlijst[Player.PlayerData.items[k].name] ~= nil then
+                    price = price + (Leeggoedlijst[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                    Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
+
+        Player.Functions.AddMoney("cash", price, "sold-resources")
+            TriggerClientEvent('QBCore:Notify', src, "Je hebt je leeggoed verkocht voor €"..price)
+            TriggerEvent("qb-log:server:CreateLog", "sellleeggoed", "resources", "blue", "**"..GetPlayerName(src) .. "** heeft €"..price.." Gekregen vor je leeggoed.")
+                end
+            end
+        end
+    else
+        TriggerClientEvent('QBCore:Notify', src, "Je hebt geen leeggoed!")
+    end
+
+end)
